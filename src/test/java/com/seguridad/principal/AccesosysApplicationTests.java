557deeeec733bd4e49bb98a74f6c4a1a8fb69fb4
MikeDRG <chief_mike117@hotmail.com>
2023-07-14 01:51:50 -0600

    Se crea proyecto de Login desde base de datos

---
+++ src/test/java/com/seguridad/principal/AccesosysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguridad.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesosysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
